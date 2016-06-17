1c5cb3e49fe775f2bdfa60a66c75feed92a6c3df
Hiro6634 <hiro.suyama@gmail.com>
2016-06-17 20:55:22 -0300

    Activity_Main menu

---
+++ app/src/main/java/edu/galileo/android/tipcalc/TipCalcApp.java
@@ -0,0 +1,13 @@
+package edu.galileo.android.tipcalc;
+
+import android.app.Application;
+/**
+ * Created by Hiro on 17/06/2016.
+ */
+public class TipCalcApp extends Application {
+    private final static String ABOUT_URL = "https://about.me/adriancatwlan";
+
+    public static String getAboutUrl() {
+        return ABOUT_URL;
+    }
+}
